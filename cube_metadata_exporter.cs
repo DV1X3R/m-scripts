6329b4d009b4e2f0c05446b5356cec7c746d74d6
DV1X3R <31878959+DV1X3R@users.noreply.github.com>
2018-04-26 17:49:16 +0300

    Added perspectives support

--- cube_metadata_exporter.cs
+++ cube_metadata_exporter.cs
@@ -1,4 +1,5 @@
 using Microsoft.AnalysisServices;
+using System.Collections.Generic;
 
 [Microsoft.SqlServer.Dts.Pipeline.SSISScriptComponentEntryPointAttribute]
 public class ScriptMain : UserComponent
@@ -17,13 +18,59 @@ public class ScriptMain : UserComponent
                     {
                         Output0Buffer.AddRow();
                         Output0Buffer.DatabaseName = OLAPDatabase.Name;
-                        Output0Buffer.CubeName = OLAPCube.Name;
+                        Output0Buffer.ObjectType = "Cube";
+                        Output0Buffer.ObjectName = OLAPCube.Name;
                         Output0Buffer.MeasureGroupName = OLAPMeasureGroup.Name;
                         Output0Buffer.MeasureGroupDimensionName = OLAPMeasureGroupDimension.CubeDimension.Name;
                         Output0Buffer.DimensionName = OLAPMeasureGroupDimension.CubeDimension.DimensionID;
                     }
                 }
+
+                foreach (Perspective OLAPPerspective in OLAPCube.Perspectives)
+                {
+                    var perspectiveDimensions = new List<CubeDimension>();
+                    var mappedDimensions = new List<CubeDimension>();
+
+                    foreach (PerspectiveDimension OLAPPerspectiveDimension in OLAPPerspective.Dimensions)
+                    {
+                        perspectiveDimensions.Add(OLAPPerspectiveDimension.CubeDimension);
+                    }
+
+                    foreach (PerspectiveMeasureGroup OLAPPerspectiveMeasureGroup in OLAPPerspective.MeasureGroups)
+                    {
+                        foreach (MeasureGroupDimension OLAPMeasureGroupDimension in OLAPPerspectiveMeasureGroup.MeasureGroup.Dimensions)
+                        {
+                            if (perspectiveDimensions.Exists((x) => x.Equals(OLAPMeasureGroupDimension.CubeDimension)))
+                            {
+                                mappedDimensions.Add(OLAPMeasureGroupDimension.CubeDimension);
+
+                                Output0Buffer.AddRow();
+                                Output0Buffer.DatabaseName = OLAPDatabase.Name;
+                                Output0Buffer.ObjectType = "Perspective";
+                                Output0Buffer.ObjectName = OLAPPerspective.Name;
+                                Output0Buffer.MeasureGroupName = OLAPPerspectiveMeasureGroup.MeasureGroup.Name;
+                                Output0Buffer.MeasureGroupDimensionName = OLAPMeasureGroupDimension.CubeDimension.Name;
+                                Output0Buffer.DimensionName = OLAPMeasureGroupDimension.CubeDimension.DimensionID;
+                            }
+                        }
+                    }
+
+                    perspectiveDimensions.RemoveAll((x) => mappedDimensions.Contains(x));
+
+                    foreach(CubeDimension UnmappedDimension in perspectiveDimensions)
+                    {
+                        Output0Buffer.AddRow();
+                        Output0Buffer.DatabaseName = OLAPDatabase.Name;
+                        Output0Buffer.ObjectType = "Perspective";
+                        Output0Buffer.ObjectName = OLAPPerspective.Name;
+                        Output0Buffer.MeasureGroupName = null;
+                        Output0Buffer.MeasureGroupDimensionName = UnmappedDimension.Name;
+                        Output0Buffer.DimensionName = UnmappedDimension.DimensionID;
+                    }
+
+                }
             }
         }
     }
+
 }

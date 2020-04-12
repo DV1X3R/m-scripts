ed1e5b47b1e64541ba507ff857d3bd996281af03
dv1x3r <31878959+dv1x3r@users.noreply.github.com>
2020-04-13 00:37:39 +0300

    Add files via upload

---
+++ wsdl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WSDL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wsdl = "?WSDL";
+            var token = "";
+            var connectorId = "";
+
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport) { MaxReceivedMessageSize = 2097152, MaxBufferSize = 2097152 };
+
+            var soap = new AfasService.ConnectorAppGetSoapClient(binding, new EndpointAddress(wsdl));
+            var resp = soap.GetData(token, connectorId, "", -1, -1);
+            Console.WriteLine(resp);
+            Console.ReadLine();
+        }
+    }
+}
